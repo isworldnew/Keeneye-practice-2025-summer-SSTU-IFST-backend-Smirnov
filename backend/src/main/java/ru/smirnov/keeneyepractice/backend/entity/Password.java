f279f2aee9cdb6373b2c8c89a3c5de98c268d05c
Ivan <isworldnew@mail.ru>
2025-07-09 18:19:29 +0400

    password entity temporary disabled

--- backend/src/main/java/ru/smirnov/keeneyepractice/backend/entity/Password.java
+++ backend/src/main/java/ru/smirnov/keeneyepractice/backend/entity/Password.java
@@ -1,31 +1,31 @@
-package ru.smirnov.keeneyepractice.backend.entity;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-@Entity
-@Table(name = "passwords")
-@Data @NoArgsConstructor @AllArgsConstructor
-public class Password {
-
-    static BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    private String password;
-
-    public Password(String password) {
-        this.password = passwordEncoder.encode(password);
-    }
-
-    @JsonIgnore
-    private void setPasswordWithEncoding(String password) {
-        this.password = passwordEncoder.encode(password);
-    }
-
-}
+//package ru.smirnov.keeneyepractice.backend.entity;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//@Entity
+//@Table(name = "passwords")
+//@Data @NoArgsConstructor @AllArgsConstructor
+//public class Password {
+//
+//    static BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//
+//    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long id;
+//
+//    private String password;
+//
+//    public Password(String password) {
+//        this.password = passwordEncoder.encode(password);
+//    }
+//
+//    @JsonIgnore
+//    private void setPasswordWithEncoding(String password) {
+//        this.password = passwordEncoder.encode(password);
+//    }
+//
+//}
